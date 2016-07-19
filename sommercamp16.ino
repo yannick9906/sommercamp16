725b0f183f8032a96f25a07dca6f43263c5c8af9
Yannick Felix <yannick.felix1999@gmail.com>
2016-07-19 17:21:29 +0200

    * Menu w/ Items * Toggling through the menu * Reset * Mapgeneration?

--- sommercamp16.ino
+++ sommercamp16.ino
@@ -1,6 +1,7 @@
 #include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_HMC5883_U.h>
+#include <assert.h>
+//#include <Adafruit_Sensor.h>
+//#include <Adafruit_HMC5883_U.h>
 #include "Adafruit_LEDBackpack.h"
 #include "Adafruit_GFX.h"
 
@@ -8,55 +9,103 @@
 Adafruit_BicolorMatrix matrix = Adafruit_BicolorMatrix();
 
 /* Assign a unique ID to this sensor at the same time */
-Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
+//Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
 
+static const uint8_t PROGMEM
+        menu_0[] = {
+            B00011000,
+            B00000000,
+            B00000000,
+            B00000000,
+            B00000000,
+            B00000000,
+            B00000000,
+            B00011000 },
+        menu_1[] = {
+            B00000000,
+            B00000000,
+            B00011000,
+            B00111100,
+            B00111100,
+            B01111110,
+            B00000000,
+            B00000000 },
+        menu_2[] = {
+            B00000000,
+            B00000000,
+            B01010101,
+            B01010101,
+            B01010101,
+            B01010101,
+            B00000000,
+            B00000000 },
+        menu_3[] = {
+            B00000000,
+            B00011110,
+            B00000010,
+            B01000010,
+            B11100010,
+            B01000010,
+            B01111110,
+            B00000000 },
+        menu_pause[] = {
+            B00000000,
+            B01111110,
+            B01111110,
+            B00000000,
+            B00000000,
+            B01111110,
+            B01111110,
+            B00000000 };
 
-char mode = "START";
-enum displayableModes {"GAME", "CREDITS", "RESTART"};
-int displayedMode = 0;
+static uint8_t mapArray[7] ={B10000001};
+
+enum displayableModes {M_PLAY, M_CREDITS, M_RESTART};
+enum modes {MENU, GAME, CREDITS, BREAK, START};
+
+int currModeSel = M_PLAY;
 int leftBorderPosition = 0;
 int rightBorderPosition = 7;
 int playerPosition = 4;
-bool inMenu = false;
-bool inGame = false;
-bool inCredits = false;
+int currMode = START;
 long buttonPressedTime = 0;
 bool buttonPressed = false;
 
-const char lightSensor = "A3"
+const char lightSensor = A3;
 const int tasterPin = 13;
 
-//Map
-static const uint8_t PROGMEM mapArray[7] ={B10000001};
+bool buttonShortPressed(){
+    bool pressed = digitalRead(tasterPin) == LOW;
 
-boolean buttonShortPressed(){
-  //Gibt zurueck, ob der Knopf kurz gedrueckt wurde
-  bool pressed = digitalRead(tasterPin) == LOW;
-  if(pressed && !buttonPressed){
-    buttonPressedTime = millis();
-    buttonPressed = true;
-    return false;
-  }else if(!pressed && buttonPressed){
-    buttonPressed = false;
-    if((buttonPressedTime - millis())<100){
-      return true;
+    if(pressed && !buttonPressed) {
+        buttonPressed = true;
+        buttonPressedTime = millis();
+        return false;
+    } else if(!pressed && buttonPressed) {
+        Serial.println((millis() - buttonPressedTime));
+        if((millis() - buttonPressedTime) <= 500)  {
+            buttonPressed = false;
+            return true;
+        } else return false;
     }
-  }
+    return false;
 }
 
-boolean buttonLongPressed(){
-  //Gibt zurueck, ob der Knopf lang gedrueckt wurde
-  bool pressed = digitalRead(tasterPin) == LOW;
-  if(pressed && !buttonPressed){
-    buttonPressedTime = millis();
-    buttonPressed = true;
-    return false;
-  }else if(!pressed && buttonPressed){
-    buttonPressed = false;
-    if((buttonPressedTime - millis())>100){
-      return true;
+bool buttonLongPressed(){
+    bool pressed = digitalRead(tasterPin) == LOW;
+
+    if(pressed && !buttonPressed) {
+        buttonPressed = true;
+        buttonPressedTime = millis();
+        return false;
+    } else if(!pressed && buttonPressed) {
+        Serial.println((millis() - buttonPressedTime));
+        if((millis() - buttonPressedTime) > 500)  {
+            buttonPressed = false;
+            return true;
+        } else return false;
     }
-  }
+    return false;
 }
 
 /* Returns an integer in the range [0, n).
@@ -82,9 +131,9 @@ int randint(int n) {
   }
 }
 
-int[] getSensorData(){
+float getSensorData() {
   /* Get a new sensor event */ 
-  sensors_event_t event; 
+  /*sensors_event_t event;
   mag.getEvent(&event);
   // Hold the module so that Z is pointing 'up' and you can measure the heading with x&y
   // Calculate heading when the magnetometer is level, then correct for signs of axis.
@@ -106,107 +155,109 @@ int[] getSensorData(){
    
   // Convert radians to degrees for readability.
   float headingDegrees = heading * 180/M_PI; 
-  
-  return headingDegrees
+  */
+  return 0;
 }
 
-void displayText(char text){
-  //Zeigt einen Text auf dem Display  
-  matrix.setTextWrap(false);  // we dont want text to wrap so it scrolls nicely
-  matrix.setTextSize(1);
-  matrix.setTextColor(LED_GREEN);
-  for (int8_t x=7; x>=-36; x--) {
-    matrix.clear();
-    matrix.setCursor(x,0);
-    matrix.print(text);
-    matrix.writeDisplay();
-    delay(2000);
+void displayText(String text) {
+    //Zeigt einen Text auf dem Display
+    matrix.setTextWrap(false);  // we dont want text to wrap so it scrolls nicely
+    matrix.setTextSize(1);
+    matrix.setTextColor(LED_GREEN);
+    matrix.setRotation(3);
+    for (int8_t x = 7; x >= -36; x--) {
+        matrix.clear();
+        matrix.setCursor(x, 0);
+        matrix.print(text);
+        matrix.writeDisplay();
+        delay(100);
+    }
+    matrix.setRotation(0);
 }
 
-int generateNewMapLine(){
-  chance = randint(2);
-  int newLBorder, newRBorder;
+int generateNewMapLine() {
+    int chance = randint(2);
+    int newLBorder, newRBorder;
     switch(chance){
-      case 0:
-        newLBorder = leftBorderPosition--;
-        break;
-      case 1:
-        newLBorder = leftBorderPosition;
-        break;
-      case 2:
-        newLBorder = leftBorderPosition++;
-        break;
+        case 0:
+            newLBorder = leftBorderPosition--;
+            break;
+        case 1:
+            newLBorder = leftBorderPosition;
+            break;
+        case 2:
+            newLBorder = leftBorderPosition++;
+            break;
     }
     newRBorder = newLBorder + 4;
     if(newLBorder < 0 || newRBorder > 7){
-      return generateNewMapLine()
+        return generateNewMapLine();
     }
-    static const uint8_t PROGMEM temp = {B00000000}
-    for ( i=0; i<8; i++ ){
-      if(i == newLBorder || i == newRBorder){
-        temp ^= (-x ^ temp) & (1 << n);
-      }
-  }
+    static uint8_t temp = {B00000000};
+    for (int i=0; i<8; i++ ){
+        if(i == newLBorder || i == newRBorder){
+            temp ^= 1 << i;
+        }
+    }
 }
 
 void preGenerateMap(){
-  //generiert den Anfang der Map
-  static const uint8_t PROGMEM 
-  temp ={B10000001,
-  B10000001,
-  B10000001,
-  B10000001,
-  B10000001,
-  B10000001,
-  B10000001};
-  for ( i=0; i<7; i++ ){
-    temp[i] = generateNewMapLine()
-  }
-  mapArray = temp
+    //generiert den Anfang der Map
+    Serial.println("Startgen");
+    for (int i=0; i<7; i++){
+        mapArray[i] = (uint8_t) generateNewMapLine();
+    }
+    Serial.println("Endgen.");
 }
 
 void updateMap(){
-  
+
 }
 
 void displayMap(){
-  matrix.clear();
-  matrix.drawBitmap(0, 0, mapArray, 8, 8, LED_GREEN);
-  matrix.writeDisplay();
-  delay(500);
+    matrix.clear();
+    matrix.drawBitmap(0, 0, mapArray, 8, 8, LED_GREEN);
+    matrix.writeDisplay();
+    delay(500);
 }
 
-void startUp(){
-  //Startet ein neues Spiel
-  displayText("Willkommen!");
-  inMenu = true;
-  mode = "MENU";
-  preGenerateMap();
+void startUp() {
+    //Startet ein neues Spiel
+    displayText("Heyho!");
+    preGenerateMap();
+    currMode = MENU;
 }
 
-void reset(){
-  //Resettet alle Variablen auf die Startwerte
-  mode = "START";
-  leftBorderPosition = 0;
-  rightBorderPosition = 7;
-  playerPosition = 4;
-  inMenu = false;
-  inGame = false;
-  startUp();
-}
-
-boolean isLight(){
-  //Gibt zurueck ob Helligkeitssensor aktiv
+bool isLight(){
+  /*//Gibt zurueck ob Helligkeitssensor aktiv
   int light = analogRead(lightSensor);
   Serial.println(light);
   if(light > 0){
     return true;
-  }
+  }*/
   return false;
 }
 
-void displaySensorDetails(void){
-  sensor_t sensor;
+void reset(){
+    //Resettet alle Variablen auf die Startwerte
+    matrix.clear();
+    matrix.drawRect(0,0,8,8, LED_RED);
+    matrix.drawRect(1,1,6,6, LED_RED);
+    matrix.drawRect(2,2,4,4, LED_RED);
+    matrix.drawRect(3,3,2,2, LED_RED);
+    matrix.writeDisplay();
+    delay(500);
+
+    currMode = START;
+    currModeSel = M_PLAY;
+    leftBorderPosition = 0;
+    rightBorderPosition = 7;
+    playerPosition = 4;
+    startUp();
+}
+
+void displaySensorDetails(){
+  /*sensor_t sensor;
   mag.getSensor(&sensor);
   Serial.println("------------------------------------");
   Serial.print  ("Sensor:       "); Serial.println(sensor.name);
@@ -217,91 +268,96 @@ void displaySensorDetails(void){
   Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" uT");  
   Serial.println("------------------------------------");
   Serial.println("");
-  delay(500);
+  delay(500);*/
+}
+
+void loopMenu() {
+    if(buttonShortPressed()) {
+        currModeSel = (currModeSel+1) % 3;
+    }
+    if(buttonLongPressed()) {
+        switch(currModeSel) {
+            case M_PLAY:
+                currMode = GAME;
+                break;
+            case M_CREDITS:
+                currMode = CREDITS;
+                break;
+            case M_RESTART:
+                reset();
+                break;
+        }
+    }
+
+    switch(currModeSel) {
+        case M_PLAY:
+            matrix.clear();
+            matrix.drawBitmap(0, 0, menu_0, 8, 8, LED_GREEN);
+            matrix.drawBitmap(0, 0, menu_1, 8, 8, LED_RED);
+            matrix.writeDisplay();
+            break;
+        case M_CREDITS:
+            matrix.clear();
+            matrix.drawBitmap(0, 0, menu_0, 8, 8, LED_GREEN);
+            matrix.drawBitmap(0, 0, menu_2, 8, 8, LED_RED);
+            matrix.writeDisplay();
+            break;
+        case M_RESTART:
+            matrix.clear();
+            matrix.drawBitmap(0, 0, menu_0, 8, 8, LED_GREEN);
+            matrix.drawBitmap(0, 0, menu_3, 8, 8, LED_RED);
+            matrix.writeDisplay();
+            break;
+    }
+}
+
+void loopGame() {
+    updateMap();
+    displayMap();
+    if(isLight()) currMode = BREAK;
+    if(leftBorderPosition >= playerPosition >= rightBorderPosition)
+        currMode = MENU;
+}
+
+void loopBreak() {
+    matrix.clear();
+    matrix.drawBitmap(0, 0, menu_pause, 8, 8, LED_YELLOW);
+    matrix.writeDisplay();
+    if(!isLight()) currMode = GAME;
 }
 
 
 void setup() {
-  //Init
-  Serial.begin(9600);
-  //Tasterinitialisierung
-  pinMode(tasterPin, INPUT);
-  digitalWrite(tasterPin, HIGH);
-  //Magnetometerinitialisierung
-  Serial.println("HMC5883 Magnetometer Test"); Serial.println("");
-  /* Initialise the sensor */
-  if(!mag.begin()){
-    /* There was a problem detecting the HMC5883 ... check your connections */
+    //Init
+    Serial.begin(9600);
+    //Tasterinitialisierung
+    pinMode(tasterPin, INPUT);
+    digitalWrite(tasterPin, HIGH);
+
+    Serial.println("Our awesome Racinggame"); Serial.println("");
+    /* Initialise the sensor */
+    /*if(!mag.begin()){
+    //There was a problem detecting the HMC5883 ... check your connections
     Serial.println("Ooops, no HMC5883 detected ... Check your wiring!");
     while(1);
-  }
-  //Displayinitialisierung
-  matrix.begin(0x70);
-  // Yannick macht hier die Init rein
-  startUp();
+    }*/
+    //Displayinitialisierung
+    matrix.begin(0x70);
+    startUp();
 }
 
 void loop() {
-  //Unterbricht das Spiel, falls der Helligkeitssensor nicht abgedeckt ist
-  if(inGame){
-    if(isLight()){
-      mode = "BREAK";
-    }else{
-      mode = "GAME";
+    switch(currMode) {
+        case MENU:
+            loopMenu();
+            break;
+        case GAME:
+            loopGame();
+            break;
+        case BREAK:
+            loopBreak();
+            break;
     }
-  }
-  //Spielt die einzelnen Modi ab
-  switch(mode){
-    case "MENU":
-      //Zeigt das Menu
-      displayText(displayableModes[displayedMode]);
-      break;
-    case "BREAK":
-      //Zeigt Pause
-      displayText("PAUSE");
-      break;
-    case "GAME":
-      //Gameschleife
-      displayMap();
-      updateMap();
-      if(leftBorderPosition>=playerPosition>=rightBorderPosition){
-        currMode = MENU;
-      }
-      break;
-    case "CREDITS":
-      //Abspielen der Credits
-      inCredits = true;
-      displayText("Credits");
-      inCredits = false;
-      mode = "MENU";
-      break;
-    //Startet das System neu
-    case "RESTART":
-      reset()
-      break;
-    default:
-      //Falls unbekannter Modus ausgewaehlt ist, zurueck ins Menu
-      mode = "MENU";
-      break;
-    }
-   //Spiel abbrechen, falls der Knopf lange gedrueckt wurde.
-   if(inGame && buttonLongPressed()){
-    mode = "MENU";
-    inGame = false;
-    inMenu = true;
-   }
-   //Angezeigten Modus durchwechseln
-   if(inMenu && buttonShortPressed()){
-    if(displayedMode++ < displayableModes.length()-1){
-      displayedMode++;
-    }else{
-      displayedMode = 0;
-    }
-   }
-   //Angezeigten Modus auswaehlen
-   if(inMenu && butttonLongPressed()){
-    mode = displayableModes[displayedMode]
-   }
 }
 
 
